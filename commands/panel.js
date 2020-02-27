a560951517b16287c8dc0bc066bcd66510d8cb9e
Jacob Song <jacobsong@Jacobs-MacBook-Pro.local>
2020-02-26 18:54:24 -0700

    update events

--- commands/panel.js
+++ commands/panel.js
@@ -37,7 +37,8 @@ module.exports = {
 
       if (players.length) {
         players.forEach((player, i) => {
-          embed.addField(`${i + 1}. ${player.discordName}`, `completed ${player.miles.toFixed(2)} miles`);
+          //embed.addField(`${i + 1}. ${player.discordName}`, `completed ${player.miles.toFixed(2)} water goals`);
+          embed.addField(`${i + 1}. ${player.discordName}`, `completed ${player.miles} water goals`);
         })
       }
 
--- commands/update.js
+++ commands/update.js
@@ -15,6 +15,7 @@ module.exports = {
   async execute(msg, args) {
     const today = new Date();
     const event = await Event.findOne({});
+    const validEmojis = ['680863088450600970', '680853613366870090'];
     
     if (event === null) {
       msg.reply("No event found");
@@ -24,32 +25,56 @@ module.exports = {
       return;
     }
 
-    const isResponseValid = miles => {
-      const x = parseFloat(miles)
-      if (isNaN(x)) return false
-      else return true
-    }
+    // const isResponseValid = miles => {
+    //   const x = parseFloat(miles)
+    //   if (isNaN(x)) return false
+    //   else return true
+    // }
     
-    const filter = (response) => {
-      return msg.author.id === response.author.id;
+    const filter = (reaction, user) => {
+      return validEmojis.includes(reaction.emoji.id) && msg.author.id === user.id;
     }
+
+    // const filter = (response) => {
+    //   return msg.author.id === response.author.id;
+    // }
     
     try {
-      msg.reply("How many miles did you complete?");
-      let miles = await msg.channel.awaitMessages(filter, { max: 1, time: 60000, errors: ["time"] });
-      while (isResponseValid(miles.first().content) === false) {
-        msg.reply("How many miles did you complete?");
-        miles = await msg.channel.awaitMessages(filter, { max: 1, time: 60000, errors: ["time"] });
+      const msg2 = await msg.reply("Did you meet your Drinking Goal today?");
+      await msg2.react(msg.guild.emojis.get('680863088450600970'));
+      await msg2.react(msg.guild.emojis.get('680853613366870090'));
+      const collected = await msg2.awaitReactions(filter, { max: 1, time: 60000, errors: ["time"] });
+      
+      if (collected.size) {
+        const choice = validEmojis.indexOf(collected.first().emoji.id);
+        if (choice === 0 || undefined) { return; }
+        
+        await Player.updateOne(
+          { discordId: msg.author.id },
+          {
+            $set: { discordId: msg.author.id, discordName: msg.author.tag, discordAvatar: msg.author.avatarURL },
+            //$inc: { miles: parseFloat(miles.first().content) }
+            $inc: { miles: 1 }
+          },
+          { upsert: true }
+        );
       }
+      // msg.reply("How many ounces did you drink?");
+      // let miles = await msg.channel.awaitMessages(filter, { max: 1, time: 60000, errors: ["time"] });
+      // while (isResponseValid(miles.first().content) === false) {
+      //   msg.reply("How many ounces did you drink?");
+      //   miles = await msg.channel.awaitMessages(filter, { max: 1, time: 60000, errors: ["time"] });
+      // }
 
-      await Player.updateOne(
-        { discordId: msg.author.id },
-        {
-          $set: { discordId: msg.author.id, discordName: msg.author.tag, discordAvatar: msg.author.avatarURL },
-          $inc: { miles: parseFloat(miles.first().content) }
-        },
-        { upsert: true }
-      );
+      // await Player.updateOne(
+      //   { discordId: msg.author.id },
+      //   {
+      //     $set: { discordId: msg.author.id, discordName: msg.author.tag, discordAvatar: msg.author.avatarURL },
+      //     //$inc: { miles: parseFloat(miles.first().content) }
+      //     $inc: { miles: 1 }
+      //   },
+      //   { upsert: true }
+      // );
     } catch (e) {
       return;
     }
